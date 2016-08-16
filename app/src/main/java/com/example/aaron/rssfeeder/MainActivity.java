79769f90dae0881cb7a961d914fe5f142045c853
pokerplayer123 <z969aaron969z@gmail.com>
2016-08-16 15:06:00 +1000

    2

--- app/src/main/java/com/example/aaron/rssfeeder/MainActivity.java
+++ app/src/main/java/com/example/aaron/rssfeeder/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aaron.rssfeeder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    private class DownloadData extends AsyncTas<String, Void, String> {
+
+        private String mFileContents;
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXMLFile(params[0]);
+            if (mFileContents == null) {
+                Log.d("DownloadData", "Error Downloading");
+            }
+            return mFileContents;
+        }
+        }
+    }
+
